2a46287cea7c8954b7c759161afa3b05331739ea
Ervina Kusnanda <ervinaksnnda@gmail.com>
2025-11-11 12:26:45 +0700

    Delete katalog_produk directory

--- katalog_produk/config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_HOST', 'localhost');         // alamat server database
-define('DB_NAME', 'katalog_produk');    // nama data base yg dipakai
-define('DB_USER', 'root');              // username buat masuk kedatabase
-define('DB_PASS', '');
-?>
